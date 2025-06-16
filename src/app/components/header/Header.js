1b8988af248d18a809fc996ae9e64fa921dc6c3d
Farooq <30216781+mianfarooqazam@users.noreply.github.com>
2025-06-16 13:22:17 +0500

    Header Ui Improved
    logo Updated

--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -1,5 +1,5 @@
 import { User, Settings, Bell } from 'lucide-react';
-import { Badge, Box } from '@mui/material';
+import { Badge, Box, Typography } from '@mui/material';
 import Image from 'next/image';
 
 export default function Header() {
@@ -11,7 +11,7 @@ export default function Header() {
                 p: 2,
                 display: 'flex',
                 alignItems: 'center',
-                justifyContent: 'space-between', 
+                justifyContent: 'space-between',
                 height: 64,
                 gridColumn: 'span 9'
             }}
@@ -21,23 +21,16 @@ export default function Header() {
                 <Image 
                     src="/lca-logo.png" 
                     alt="LCA Logo" 
-                    width={40}
-                    height={40}
+                    width={69}
+                    height={69}
                     style={{
                         objectFit: 'contain',
                         backgroundColor: 'transparent'
                     }}
                 />
-                <Image 
-                    src="/berc-logo.png" 
-                    alt="BERC Logo" 
-                    width={40}
-                    height={40}
-                    style={{
-                        objectFit: 'contain',
-                        backgroundColor: 'transparent'
-                    }}
-                />
+                <Typography variant="h6" sx={{ fontWeight: "bold", color: '#5BB045' }}>
+                   Life Cycle Analysis
+                </Typography>
             </Box>
 
             {/* Icons on the right */}
