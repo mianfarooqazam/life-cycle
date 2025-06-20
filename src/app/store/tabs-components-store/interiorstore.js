93676bcdc5ce0c4f439f93834d66f04a25908754
Farooq <30216781+mianfarooqazam@users.noreply.github.com>
2025-06-20 14:14:33 +0500

    Persist added
    Added to building interior, exterior and slab

--- src/app/store/tabs-components-store/interiorstore.js
+++ src/app/store/tabs-components-store/interiorstore.js
@@ -1,20 +1,28 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-export const useInteriorStore = create((set) => ({
-  interiors: [],
-  editingInterior: null,
-  addInterior: (interior) => set((state) => ({
-    interiors: [...state.interiors, { ...interior, id: Date.now() }],
-  })),
-  updateInterior: (id, updatedInterior) => set((state) => ({
-    interiors: state.interiors.map((interior) =>
-      interior.id === id ? { ...interior, ...updatedInterior } : interior
-    ),
-    editingInterior: null,
-  })),
-  deleteInterior: (id) => set((state) => ({
-    interiors: state.interiors.filter((interior) => interior.id !== id),
-  })),
-  setEditingInterior: (interior) => set({ editingInterior: interior }),
-  clearEditingInterior: () => set({ editingInterior: null }),
-})); 
+export const useInteriorStore = create(
+  persist(
+    (set) => ({
+      interiors: [],
+      editingInterior: null,
+      addInterior: (interior) => set((state) => ({
+        interiors: [...state.interiors, { ...interior, id: Date.now() }],
+      })),
+      updateInterior: (id, updatedInterior) => set((state) => ({
+        interiors: state.interiors.map((interior) =>
+          interior.id === id ? { ...interior, ...updatedInterior } : interior
+        ),
+        editingInterior: null,
+      })),
+      deleteInterior: (id) => set((state) => ({
+        interiors: state.interiors.filter((interior) => interior.id !== id),
+      })),
+      setEditingInterior: (interior) => set({ editingInterior: interior }),
+      clearEditingInterior: () => set({ editingInterior: null }),
+    }),
+    {
+      name: 'interior-storage',
+    }
+  )
+); 
